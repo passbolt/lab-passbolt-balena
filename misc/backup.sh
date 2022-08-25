8a658c4b9c12880c79d705b057d2f8aec4b84609
Jean-Christophe Vassort <jean-christophe@passbolt.com>
2022-08-25 14:23:33 +0200

    Update backup script

--- misc/backup.sh
+++ misc/backup.sh
@@ -2,6 +2,8 @@
 
 DATE=$(date "+%Y%m%d%H%M%S")
 
+echo "$(date): Start backup"
+
 # Dump database
 mysqldump -h db -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} | gzip > /data/${DATE}-passbolt.sql.gz
 
@@ -13,3 +15,7 @@ find /data -name "*gz" -mtime +16 -delete
 
 # Add here your rclone command to save your backups
 # Have a look at https://rclone.org/#providers
+
+echo "Backup done, sleeping for ${BACKUP_INTERVAL} seconds"
+
+sleep ${BACKUP_INTERVAL}
